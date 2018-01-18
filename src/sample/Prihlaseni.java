4d13feb1500b8145946bd2b7bf06c11342b4749a
fils01 <32477991+fils01@users.noreply.github.com>
2018-01-18 01:27:33 +0100

    rozdělané přihlášení, nevím jak dál s těmi vracenými instancemi správce/uživatele

--- src/sample/Prihlaseni.java
+++ src/sample/Prihlaseni.java
@@ -1,10 +1,30 @@
 package sample;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+
 public class Prihlaseni {
 
+
     public static Osoba prihlasitSe(String username, String heslo){
+
+        //vůbec nevím jak s tím propojením s db
+        String hesloDb = "";
+        String status = "";
+
+        if(BCrypt.checkpw(heslo, hesloDb)) {
+            if (status.equals("spravce")) {
+                return Spravce;
+            } else {
+                return Zakaznik;
+            }
+
+        } else {
+            return null;
+        }
+
         // Ověří, zda sedí heslo a vrátí instanci uživatele/správce pokud ano, pokud ne vrátí null
-        return null;
+
     }
 
 }
