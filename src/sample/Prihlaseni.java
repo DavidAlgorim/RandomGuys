ea910755905953ec128851077fb546331161c0a9
fils01 <32477991+fils01@users.noreply.github.com>
2018-01-18 01:34:41 +0100

    rozdělané přihlášení, nevím jak dál s těmi vracenými instancemi správce/uživatele

--- src/sample/Prihlaseni.java
+++ src/sample/Prihlaseni.java
@@ -1,13 +1,22 @@
 package sample;
 
 import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
 
 
 public class Prihlaseni {
 
 
     public static Osoba prihlasitSe(String username, String heslo){
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/jdbc_db");
 
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         //vůbec nevím jak s tím propojením s db
         String hesloDb = "";
         String status = "";
