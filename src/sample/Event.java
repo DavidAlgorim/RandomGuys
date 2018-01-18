1c9620e5d421b0618ce6d725496f18dcf8f0787e
lasj00 <32467217+lasj00@users.noreply.github.com>
2018-01-18 16:18:28 +0100

    Pridano nacitani dat o eventu z DB
    Pridano nacitani dat o eventu z DB

--- src/sample/Event.java
+++ src/sample/Event.java
@@ -1,5 +1,12 @@
 package sample;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+
+
 public class Event {
 
     private String nazev;
@@ -9,7 +16,10 @@ public class Event {
     private int zvyhodnenaCena;
     private int kapacita;
     private String popis;
-
+    
+    private ObservableList<Event> dataEventy;
+    
+    
     // Konstruktor pro nově vytvářený event
     public Event(String nazev, Organizator organizator, Misto misto, int cena, int zvyhodnenaCena, int kapacita,
                  String popis){
@@ -21,16 +31,36 @@ public class Event {
         this.kapacita = kapacita;
         this.popis = popis;
     }
-
+    
     // Konstruktor pro zobrazování už vzniklého eventu
     public Event(String nazev){
         this.nazev = nazev;
-        nactiUdajeEventu();
+        ResultSet rs = null;
+        nactiUdajeEventu(rs);
     }
 
-    private void nactiUdajeEventu(){
-        // Načte zbylé atributy eventu z databáze podle jeho názvu
+    private void nactiUdajeEventu(ResultSet rs){
+        try {
+            while (rs.next()) {
+                String nazev = rs.getString("nazev");
+                Organizator organizator  = getOrganizator();
+                Misto misto = getMisto();
+                int cena = rs.getInt("cena");
+                int zvyhodnenaCena = rs.getInt("zvyhodnenaCena");
+                int kapacita = rs.getInt("kapacita");
+                String popis = rs.getString("popis");
+
+                dataEventy.add(new Event(nazev, organizator, misto, cena, zvyhodnenaCena, kapacita, popis));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Event.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
+    
+        // Načte zbylé atributy eventu z databáze podle jeho názvu
+    
 
     public String getNazev() {
         return nazev;
