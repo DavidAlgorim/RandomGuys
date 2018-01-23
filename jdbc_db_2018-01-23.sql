fd35850255ee0044ed83bf16ace18a66ffc29343
fils01 <32477991+fils01@users.noreply.github.com>
2018-01-24 00:54:20 +0100

    db

--- jdbc_db_2018-01-23.sql
+++ jdbc_db_2018-01-23.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.42)
 # Database: jdbc_db
-# Generation Time: 2018-01-21 21:48:12 +0000
+# Generation Time: 2018-01-23 19:10:59 +0000
 # ************************************************************
 
 
@@ -41,6 +41,15 @@ CREATE TABLE `event` (
   CONSTRAINT `event_ibfk_3` FOREIGN KEY (`id_organizator`) REFERENCES `organizator` (`id_organizator`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+
+INSERT INTO `event` (`id_event`, `nazev`, `id_misto`, `cena`, `zvyhodnena_cena`, `kapacita`, `popis`, `id_organizator`)
+VALUES
+	(10,'Rock For People 2018',1,1750,1300,5000,'Krátký popis eventu',1);
+
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table hodnoceni
@@ -67,7 +76,7 @@ DROP TABLE IF EXISTS `listek`;
 
 CREATE TABLE `listek` (
   `id_listek` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `zakaznik` varchar(255) NOT NULL DEFAULT '',
+  `zakaznik` varchar(255) DEFAULT '',
   `zvyhodneni` tinyint(1) NOT NULL,
   `id_event` int(11) unsigned NOT NULL,
   `id_osoba` int(11) unsigned DEFAULT NULL,
@@ -89,11 +98,20 @@ CREATE TABLE `misto` (
   `id_misto` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `nazev` varchar(255) NOT NULL DEFAULT '',
   `adresa` varchar(255) NOT NULL DEFAULT '',
-  `username` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
   `telefon` int(11) NOT NULL,
   PRIMARY KEY (`id_misto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `misto` WRITE;
+/*!40000 ALTER TABLE `misto` DISABLE KEYS */;
+
+INSERT INTO `misto` (`id_misto`, `nazev`, `adresa`, `email`, `telefon`)
+VALUES
+	(1,'Festivalpark','Letiště Hradec Králové','aaab@ccc.com',444555666);
+
+/*!40000 ALTER TABLE `misto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table organizator
@@ -104,11 +122,20 @@ DROP TABLE IF EXISTS `organizator`;
 CREATE TABLE `organizator` (
   `id_organizator` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `jmeno` varchar(255) NOT NULL DEFAULT '',
-  `username` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
   `telefon` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_organizator`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `organizator` WRITE;
+/*!40000 ALTER TABLE `organizator` DISABLE KEYS */;
+
+INSERT INTO `organizator` (`id_organizator`, `jmeno`, `email`, `telefon`)
+VALUES
+	(1,'RFP','rfp@rfp.cz',111222333);
+
+/*!40000 ALTER TABLE `organizator` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table osoba
@@ -119,41 +146,27 @@ DROP TABLE IF EXISTS `osoba`;
 CREATE TABLE `osoba` (
   `id_osoba` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `jmeno` varchar(255) NOT NULL DEFAULT '',
-  `username` varchar(255) NOT NULL DEFAULT '',
-  `username` varchar(100) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `username` varchar(100) NOT NULL UNIQUE DEFAULT '',
   `pass_hash` varchar(100) NOT NULL DEFAULT '',
   `status` varchar(255) NOT NULL DEFAULT '',
   `body` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_osoba`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `osoba` WRITE;
 /*!40000 ALTER TABLE `osoba` DISABLE KEYS */;
 
-INSERT INTO `osoba` (`id_osoba`, `jmeno`, `username`, `username`, `pass_hash`, `status`, `body`)
+INSERT INTO `osoba` (`id_osoba`, `jmeno`, `email`, `username`, `pass_hash`, `status`, `body`)
 VALUES
-	(6,'Svátík Efko','yuyy@yuyyy.com','deepSpace69','696969','uzivatel',0);
+	(9,'Jmeno Prijmeni','aaaa@bbb.cc','fghjkl','$2a$10$VdhR9h/aZ76R5ZKAKRpcNuldGp7mCa7I.8wNrLYXMHFXoXlygOsZS','uzivatel',0),
+	(11,'Hai Hai','hai@hai.cz','haihai','$2a$10$JQo0NjHBDb4XCTsZEY.4YuS3NIy9juLhBG1kLYrCouW48zOAzHLBq','uzivatel',0),
+	(12,'Yay Yuy','yyy@ggg.com','yuyyay','$2a$10$0Y1UXSoneKZbYXoV8wLTV.zc/UsUfy6ZAfaPPJYEhG1mOrTgID1g6','uzivatel',0),
+	(13,'Canon Pixma','123@gmail.com','QWRTZ','$2a$10$iBCghap9xii4C6a5R9q4H.qkT/N1IWUXBPsQ3z.QBPFeItnrgjb5i','uzivatel',0),
+	(15,'Osoba Osoba','osoba@osoba.com','osoba','$2a$10$CO5Ludphh4zD7b5WbXLrn.7XgWm4d/5Z/tIaZi5FthVp5Nmr/ZolG','uzivatel',0);
 
 /*!40000 ALTER TABLE `osoba` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-# Dump of table osoba_listky
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `osoba_listky`;
-
-CREATE TABLE `osoba_listky` (
-  `id_osoba_listky` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_listek` int(11) unsigned NOT NULL,
-  `id_osoba` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id_osoba_listky`),
-  KEY `id_listek` (`id_listek`),
-  KEY `id_osoba` (`id_osoba`),
-  CONSTRAINT `osoba_listky_ibfk_1` FOREIGN KEY (`id_listek`) REFERENCES `listek` (`id_listek`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `osoba_listky_ibfk_2` FOREIGN KEY (`id_osoba`) REFERENCES `osoba` (`id_osoba`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 
