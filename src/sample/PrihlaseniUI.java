ecc45f092a700f6d6efb753d29d4311769eba64d
fils01 <32477991+fils01@users.noreply.github.com>
2018-01-23 17:56:42 +0100

    hashování, solení hesla + clear(); po provedení registrace

--- src/sample/PrihlaseniUI.java
+++ src/sample/PrihlaseniUI.java
@@ -3,10 +3,7 @@ package sample;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -90,6 +87,11 @@ public class PrihlaseniUI {
             getRegisterUsername().clear();
             getRegisterEmail().clear();
             getRegisterPassword().clear();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Vaše registrace byla úspěšně potvrzena.");
+            alert.showAndWait();
         });
 
         loginBackButton.setOnMouseClicked(event -> {
